b5ec2a4a1c1e2e3c74cdc0d15b264c7b65134165
David <Davidcorrea.743@gmail.com>
2025-04-05 20:07:01 -0400

    Se agregan los datos del componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { Gif } from "./components/gif";
 
 function App() {
   const [data, setData] = useState(null);
@@ -12,14 +13,17 @@ function App() {
     setData(results);
   };
 
-  console.log(data);
   return (
     <>
       <header className="App-Header">
         <h1 className="title"> App Rick and Morty</h1>
-        <button className="btn-search" onClick={dataApi}>
-          Buscar
-        </button>
+        {data ? (
+          <Gif data={data} />
+        ) : (
+          <button className="btn-search" onClick={dataApi}>
+            Buscar
+          </button>
+        )}
       </header>
     </>
   );
